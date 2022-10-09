19eed8a68950202dc5b0c5036c5402c732d0459f
Shota Sato <sugergg@gmail.com>
2022-10-09 20:18:16 +0900

    ch04

--- answer/4/4.1.2.closest_pair.py
+++ answer/4/4.1.2.closest_pair.py
@@ -6,15 +6,10 @@ I = [ [0] * 2 for i in range(N)]
 ANSWER = sys.maxsize
 
 for i in range(N):
-    x, y = map(int, input().split())
-    I[i][0] = x
-    I[i][1] = y
+    I[i][0], I[i][1] = map(int, input().split())
 
 for i in range(N):
-    for j in range(N):
-        if i == j:
-            continue
-    
+    for j in range(i+1, N):
         x = pow(I[i][0] - I[j][0], 2)
         y = pow(I[i][1] - I[j][1], 2)
         dist = math.sqrt(x + y)
